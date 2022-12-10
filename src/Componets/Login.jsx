a22ff12c582d99e293fd7c76f0af050f31be6128
RahulGupta-8319 <guptagi5050@gmail.com>
2022-12-10 11:48:26 +0530

    frontend

--- src/Componets/Login.jsx
+++ src/Componets/Login.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className="login">
+      <h1>Login</h1>
+      <form>
+        <input className="input" type="email" placeholder="Email" /> <br />
+        <br />
+        <input className="input" type="password" placeholder="Password" /><br/><br/>
+        <button className="btn">Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Componets/SignUp.jsx
+++ src/Componets/SignUp.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+
+import {useState} from 'react'
+// import { json } from 'react-router-dom'
 
 const SignUp = () => {
+  const [name , setName] = useState("")
+  const [mobile , setMobile] = useState("")
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+
+  const collectData = async (e) =>{
+    e.preventDefault()
+    console.log(name, mobile, email, password);
+    let result = await fetch("http://localhost:3001/signUp", {
+      method:"post",
+      headers: {'Content-Type': 'application/json'},
+      body:JSON.stringify({name, mobile, email, password})
+    })
+    result = await result.json()
+    console.log(result);
+  }
+
   return (
     <div className='signup'>
     <h1>Sign Up</h1>
       <form>
-        <input className='input' type="text" placeholder='Name' /><br/><br/>
-        <input className='input' type="email" placeholder='Email' /><br/><br/>
-        <input className='input' type="number" placeholder='Mobile' />  <br/><br/>
-        <input className='input' type="password" placeholder='PassWord' /><br/><br/>
-        <button className='btn'>Register</button>
+        <input className='input' type="text" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} /><br/><br/>
+        <input className='input' type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} /><br/><br/>
+        <input className='input' type="number" placeholder='Mobile' value={mobile} onChange={(e) => setMobile(e.target.value)} />  <br/><br/>
+        <input className='input' type="password" placeholder='PassWord' value={password} onChange={(e) => setPassword(e.target.value)} /><br/><br/>
+        <button className='btn' onClick={collectData}>Register</button>
       
       </form>
     </div>
